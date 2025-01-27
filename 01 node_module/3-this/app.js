30fbf661d15ee04d29da8a8dea29349342544253
saysuhyun <saysuhyun@gmail.com>
2025-01-27 15:29:05 +0900

    this = exports.module

--- 01 node_module/3-this/app.js
+++ 01 node_module/3-this/app.js
@@ -10,15 +10,15 @@ class A {
     this.num = num;
   }
   memberFunction() {
-    console.log('----- class -----');
-    console.log(this);
-    console.log(this === global);
+    console.log("----- class -----");
+    console.log(this); // 글로벌이 아니라 클래스 자기자신 A
+    console.log(this === global); // false
   }
 }
 
 const a = new A(1);
 a.memberFunction();
 
-console.log('--- global scope ---');
-console.log(this);
-console.log(this === module.exports);
+console.log("--- global scope ---");
+console.log(this); // {}
+console.log(this === module.exports); // this = 모듈에 있는 export임!
